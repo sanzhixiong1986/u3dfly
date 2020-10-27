d7f5845345844e3687e1880d26cf10c36e995c19
mac <>
2020-10-27 18:34:25 +0700

    增加poerMVC版本

---
+++ PoerMVC/Assets/Script/Main.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		//启动Facade操作
+		UnityFacade.GetInstance().StartUp();
+	}
+	
+	// Update is called once per frame
+	public void GotoNextScene () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
